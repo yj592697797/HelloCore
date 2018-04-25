d54c640053d064a60a1e81eadee412b6cf9be5e0
yaojie <yaojie@smartbi.com.cn>
2018-04-25 18:25:13 +0800

    init

---
+++ HelloCore.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCore.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloCore.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HelloCore.Core
+{
+    public class Class1
+    {
+    }
+}
